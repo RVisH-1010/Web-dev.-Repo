6f469089f38ef2335378b1fa48030350086d96df
Rohan Vishwakarama <rohanvish4you@gmail.com>
2025-02-17 07:01:50 +0000

    ex 11

--- js/ex-10.js
+++ js/ex-10.js
@@ -1,6 +1,51 @@
 // BUSSINESS NAME GENERATOR
 // adjectives: crazy amazing fire
-// shop name zebra foods engineer
-// verb bros ltd assets
-let a = Math.ceil(Math.random()*3);
-console.log(a);
+// shop name: zebra foods engineer
+// verb: bros ltd assets
+// let a = Math.ceil(Math.random()*3);
+// console.log(a);
+
+// let first,second,third
+
+// if(a==1){
+//     first="crazy";
+// }
+// else if(a==2){
+//     first = "amazing"
+// }
+// else{
+//     first = "fire"
+// }
+// // second word 
+// if(a==1){
+//     second="engine";
+// }
+// else if(a==2){
+//     second = "foods"
+// }
+// else{
+//     second = "garments"
+// }
+// //for third word
+// if(a==1){
+//     third="bros";
+// }
+// else if(a==2){
+//     third = "limited"
+// }
+// else{
+//     third = "Hub"
+// }
+// console.log(`the company nmae is:${first} ${second} ${third} `)
+
+
+// ex 11 factorial
+
+let n = prompt("enter any no.: ");
+let i =1;
+let fact = 1;
+while(i<=n){
+    fact = fact *i
+    i+=1
+}
+console.log(`${fact}`)
